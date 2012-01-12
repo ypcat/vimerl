bac4eac639451c5f8832859c6de27e11ada74be0
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2012-01-12 20:48:46 +0100

    Updates EDoc example to extract the module's functions

--- edoc_test.erl
+++ edoc_test.erl
@@ -1,16 +1,63 @@
 -module(edoc_test).
 
--compile(export_all).
+-export([run/1]).
 
-%%% -----------------------------------------------------------------------------
-%%% http://userprimary.net/posts/2011/02/16/Generating-XML-in-Erlang-Using-xmerl/
-%%% http://muharem.wordpress.com/2007/08/21/processing-xml-in-erlang/
-%%% -----------------------------------------------------------------------------
+-include_lib("xmerl/include/xmerl.hrl").
 
+run([ModName]) ->
+    run(ModName);
 run(ModName) ->
     Mod = list_to_atom(ModName),
     {File0, _} = filename:find_src(Mod),
     File = File0 ++ ".erl",
     {_, Doc} = edoc:get_doc(File),
-    Xml = xmerl:export([Doc], xmerl_xml),
-    io:format("Xml = ~p~n", [Xml]).
+    Funs = xmerl_xpath:string("/module/functions/function", Doc),
+    FunsInfo = lists:map(fun inspect_function/1, Funs),
+    FunsInfo2 = lists:keysort(1, FunsInfo),
+    lists:foreach(
+        fun({Name, Args, Return}) ->
+                io:format("~s~n", [function_info_to_str(Name, Args, Return)])
+        end,
+        FunsInfo2).
+%run(_) ->
+    %bad_module.
+
+inspect_function(Fun) ->
+    Name = get_attribute(Fun, "name"),
+    Args0 = xmerl_xpath:string("typespec/type/fun/argtypes/type", Fun),
+    Args = lists:map(fun(Arg) -> get_attribute(Arg, "name") end, Args0),
+    Return = inspect_function_return(Fun),
+    {Name, Args, Return}.
+
+inspect_function_return(Fun) ->
+    [ReturnType] = xmerl_xpath:string("typespec/type/fun/type/*", Fun),
+    simplify_return_type(xmerl_lib:simplify_element(ReturnType)).
+
+simplify_return_type({type, _, [Type]}) ->
+    simplify_return_type(Type);
+simplify_return_type({tuple, _, Types}) ->
+    Elems = lists:map(fun simplify_return_type/1, Types),
+    "{" ++ string:join(Elems, ", ") ++ "}";
+simplify_return_type({list, _, Types}) ->
+    Elems = lists:map(fun simplify_return_type/1, Types),
+    "[" ++ string:join(Elems, ", ") ++ "]";
+simplify_return_type({typevar, [{name, Name}], _}) ->
+    Name;
+simplify_return_type({atom, [{value, Val}], _}) ->
+    Val;
+simplify_return_type({abstype, _, [Type]}) ->
+    {erlangName, [{name, Name}], []} = Type,
+    Name ++ "()";
+simplify_return_type({union, _, Types}) ->
+    Elems = lists:map(fun simplify_return_type/1, Types),
+    string:join(Elems, " | ");
+simplify_return_type(_) ->
+    io:format("BIG SHIT SOMETIME HAPPENS!~n"),
+    erlang:halt().
+
+function_info_to_str(Name, Args, Return) ->
+    io_lib:format("~s(~s) -> ~s", [Name, string:join(Args, ", "), Return]).
+
+get_attribute(Elem, AttrName) ->
+    [Attr] = xmerl_xpath:string("@" ++ AttrName, Elem),
+    Attr#xmlAttribute.value.
